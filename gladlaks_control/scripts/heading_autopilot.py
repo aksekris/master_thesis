29eb7339fb452d1d0e1fa1e6237a6b3bbbbe6a29
Aksel Kristoffersen <aksekris@gmail.com>
2021-02-25 23:48:49 +0100

    Make HeadingAutopilot class

--- gladlaks_control/scripts/heading_autopilot.py
+++ gladlaks_control/scripts/heading_autopilot.py
@@ -2,11 +2,72 @@
 # Written by Aksel Kristoffersen
 
 import rospy
-from geometry_msgs.msg import PoseStamped, TwistStamped, WrenchStamped
-from pid_controller import PIDRegulator
-from tf.transformations import euler_from_quaternion
 import numpy as np
 import math
+from geometry_msgs.msg import PoseStamped, TwistStamped, WrenchStamped
+from tf.transformations import euler_from_quaternion
+from pid_controller import PIDController
+from pid_pole_placement_algorithm import pid_pole_placement_algorithm
+
+class HeadingAutopilot:
+    """Heading autopilot based on PID pole-placement"""
+
+    def __init__(self, m, d, k, omega_b, zeta, tau_sat):
+        """Initialize the autopilot
+
+        Args: 
+            m           The mass coefficient
+            d           The damper coefficient
+            k           The spring coefficient
+            omega_b     The control bandwidth
+            zeta        The relative damping ratio
+            tau_sat     The input torque saturation limit
+
+        """
+        (K_p, K_d, K_i) = pid_pole_placement_algorithm(m, d, k, omega_b, zeta)
+        self.PID = PIDController(K_p, K_d, K_i, tau_sat)
+
+    def calculate_control_torque(self, yaw_err, yaw_dt, t):
+        """Calculate the control torque for heading control using a PID controller
+
+        Args:
+            yaw_err     The yaw error
+            yaw_dt      The yaw derivative
+            t           The time now
+
+        Returns:
+            float:      The control torque tau
+
+        """
+        if yaw_err < -np.pi or yaw_err >= np.pi:
+            # Map angle to [-pi, pi)
+            yaw_err = (yaw_err + np.pi) % (2 * np.pi) - np.p
+        return self.PID.regulate(yaw_err, yaw_dt, t)
+        
+
+if __name__ == '__main__':
+    pilot = HeadingAutopilot(1, 1, 1, 1, 1, 1)
+    tau = pilot.calculate_control_torque(1, 1, 1)
+    print(tau)
+
+"""
+    def __init__(self, iterable):
+        self. = []
+        self.__update(iterable)
+
+    sub1 = rospy.Subscriber('/gladlaks/navigation_system/pose', PoseStamped, callback)
+    sub2 = rospy.Subscriber('/gladlaks/navigation_system/twist', TwistStamped, callback)
+
+    rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('heading_autopilot')
+    try:
+        HeadingAutopilot()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
 
 def callback(data):
     quaternions = data.pose.orientation
@@ -15,9 +76,7 @@ def callback(data):
 
     psi_d = math.pi
     e = ((psi-psi_d) + np.pi) % (2 * np.pi) - np.pi
-    print(e)
     u = PID.regulate(e, rospy.get_time())
-
     msg = WrenchStamped()
     msg.header.stamp = rospy.get_rostime()
     msg.header.frame_id = "gladlaks/base_link_ned"
@@ -26,10 +85,22 @@ def callback(data):
 
 if __name__ == '__main__':
     try:
-        PID = PIDRegulator(0.1, 0, 0, 1)
+        omega_b = rospy.get_param("/control_system/heading_autopilot/control_bandwidth")
+        zeta = rospy.get_param("/control_system/heading_autopilot/relative_damping_ratio")
+        M_RB = rospy.get_param("/auv_dynamics/M_RB")
+        M_A = rospy.get_param("/auv_dynamics/M_A")
+        m = M_RB[5][5]+M_A[5][5]
+        d = rospy.get_param("/auv_dynamics/D")[5]
+        k = 0
+        print(m, d, k, omega_b, zeta)
+        (K_p, K_d, K_i) = PID_pole_placement_algorithm(m, d, k, omega_b, zeta)
+        print(K_p, K_d, K_i)
+        PID = PIDRegulator(K_p, 0, K_i, 44)
         rospy.init_node('heading_autopilot', anonymous=True)
-        sub = rospy.Subscriber('/gladlaks/navigation_system/pose', PoseStamped, callback)
+        sub1 = rospy.Subscriber('/gladlaks/navigation_system/pose', PoseStamped, callback)
+        sub2 = rospy.Subscriber('/gladlaks/navigation_system/twist', TwistStamped)
         pub = rospy.Publisher('/gladlaks/thruster_manager/input_stamped', WrenchStamped, queue_size=1)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
+"""
--- gladlaks_control/scripts/pid_controller.py
+++ gladlaks_control/scripts/pid_controller.py
@@ -1,75 +1,51 @@
 #!/usr/bin/env python
-# Written by Kristoffer Rakstad Solberg, Student
-# Copyright (c) 2020 Manta AUV, Vortex NTNU.
-# All rights reserved.
+# Written by Kristoffer Rakstad Solberg
+# Contributed by Aksel Kristoffersen
 
 import numpy as np
 
-class PIDRegulator:
-	"""
-	A very basic 1D PID controller
-	"""
+class PIDController:
 
-	def __init__(self, p, i, d, sat):
-		"""
-		Initialize the PID controller by setting gains
-		and saturation
+	def __init__(self, K_p, K_d, K_i, u_sat):
+		"""Initialize the PID controller
 
 		Args:
-			p	  proportional gain
-			i	  integral gain
-			d	  derivative gain
-			sat	  saturation limit
+			K_p	  	Proportional gain
+			K_i	  	Integral gain
+			K_d	  	Derivative gain
+			u_sat	Output saturation limit
 		"""
-
-		self.p = p
-		self.i = i
-		self.d = d
-		self.sat = sat
-
+		self.K_p = K_p
+		self.K_i = K_i
+		self.K_d = K_d
+		self.u_sat = u_sat
 		self.integral = 0
-		self.prev_err = 0
+		self.prev_x_err = 0
 		self.prev_t = -1
 
-	def __str__(self):
-		"""
-		Create a formatted string containing the controller gains
-		and the saturation limit.
-		"""
-
-		msg = 'PID controller:'
-		msg += '\n\tp=%f' % self.p
-		msg += '\n\cd ti=%f' % self.i
-		msg += '\n\td=%f' % self.d
-		msg += '\n\tsat=%f' % self.sat
-		return msg
-
-	def regulate(self, err, t):
-		"""
-		Calculate the controller gain 
+	def regulate(self, x_err, x_dt, t):
+		"""Calculate the controller output
 
 		Args:
-			err	  the state error used to calculate controller gain (e)
-			t	  the current time
+			x_err	  	The state error
+			x_dt		The state derivative
+			t	  		The current time
 
 		Returns:
-			float:	The controller gain u
-		"""
+			float:		The controller output u
 
-		derr_dt = 0.0
+		"""
 		dt = t - self.prev_t
 		if self.prev_t > 0.0 and dt > 0.0:
-			derr_dt = (err - self.prev_err)/dt
-			self.integral += 0.5*(err + self.prev_err)*dt
+			self.integral += x_err*dt
 
-		u = self.p*err + self.d*derr_dt + self.i*self.integral
-
-		self.prev_err = err
+		u_unsat = -(self.K_p*x_err + self.K_d*x_dt + self.K_i*self.integral)
+		self.prev_x_err = x_err
 		self.prev_t = t
 
-		if (np.linalg.norm(u) > self.sat):
-			# controller is in saturation: limit output, reset integral
-			u = self.sat*u/np.linalg.norm(u)
-			self.integral = 0.0
-
+		if abs(u_unsat) > self.u_sat:
+			# Anti-wind-up
+			u = np.sign(u_unsat) * self.u_sat
+			self.integral += - (dt/self.K_i) * (u - u_unsat)
 		return u
+
